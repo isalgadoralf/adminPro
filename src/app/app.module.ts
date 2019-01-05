7c72c92f1591742f794d1b7a015bd50ae17a0b05
Rafael Salgado <isalgadoralf@gmail.com>
2019-01-05 09:47:51 -0400

    reorganizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
 //RUTAS
 import {APP_ROUTES} from "./app.routes";
-import { PagesComponent } from './pages/pages.component';
+//MODULES
+import {PagesModule} from "./pages/pages.module";
 import { RegisterComponent } from './login/register.component';
 
 
@@ -20,18 +14,11 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
